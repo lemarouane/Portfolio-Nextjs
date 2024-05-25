bb0bbd700e4fdca5f20bde93f9ca1ccaf42ea054
MAROUANE HADDAD <143835897+lemarouane@users.noreply.github.com>
2024-05-25 15:27:18 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <span>2023 All Rights Reserved.</span>
                 </div>
                 <div className="flex items-center">
-                    <a href='https://www.fiverr.com/codeworthy' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Marouane</span></a>
+                    <a href='#' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Marouane</span></a>
                 </div>
                 <div className="hidden md:flex items-center">
                     <div className="mr-1 text-base">
